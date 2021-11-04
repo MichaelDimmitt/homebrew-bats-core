62d8c1dfc9ff67ef69676505ae967b54cb3ae7cb
michaeldimmitt <michael.dimmitt@petco.com>
2021-11-03 21:35:12 -0400

    fix duplicate and add a depens_on

--- Formula/bats-support.rb
+++ Formula/bats-support.rb
@@ -6,6 +6,7 @@ class BatsSupport < Formula
     version "v0.3.0"
     sha256 "7815237aafeb42ddcc1b8c698fc5808026d33317d8701d5ec2396e9634e2918f"
     license "CC0 1.0 Universal"
+    depends_on "bats-core"
 
     def install
       mkdir "bats-support"
@@ -18,7 +19,6 @@ class BatsSupport < Formula
     def caveats
       <<~EOS
         To load the bats-support lib in your bats test:
-            load '#{HOMEBREW_PREFIX}/lib/bats-support/load.bash'
             load '#{HOMEBREW_PREFIX}/lib/bats-support/load.bash'
       EOS
     end

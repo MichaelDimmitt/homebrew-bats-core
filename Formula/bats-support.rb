b966d149eb60a37c7e22a5c4ec89e78ed20846b2
brokenpip3 <brokenpip3@gmail.com>
2021-11-23 23:11:46 +0100

    fix space and license to pass brew audit

--- Formula/bats-support.rb
+++ Formula/bats-support.rb
@@ -1,33 +1,33 @@
 class BatsSupport < Formula
-    desc "Supporting library for Bats test helpers"
-    homepage "https://github.com/bats-core/homebrew-bats-core"
-    url "https://github.com/bats-core/bats-support/archive/refs/tags/v0.3.0.tar.gz"
-    head "https://github.com/bats-core/bats-support.git"
-    version "v0.3.0"
-    sha256 "7815237aafeb42ddcc1b8c698fc5808026d33317d8701d5ec2396e9634e2918f"
-    license "CC0 1.0 Universal"
-    depends_on "bats-core"
+  desc "Supporting library for Bats test helpers"
+  homepage "https://github.com/bats-core/homebrew-bats-core"
+  url "https://github.com/bats-core/bats-support/archive/refs/tags/v0.3.0.tar.gz"
+  head "https://github.com/bats-core/bats-support.git"
+  version "0.3.0"
+  sha256 "7815237aafeb42ddcc1b8c698fc5808026d33317d8701d5ec2396e9634e2918f"
+  license "MIT"
+  depends_on "bats-core"
 
-    def install
+  def install
       mkdir "bats-support"
       mv "load.bash", "bats-support/"
       mv "src", "bats-support/"
       mv "test", "bats-support/"
       lib.install "bats-support"
-    end
+  end
 
-    def caveats
+  def caveats
       <<~EOS
 
         To load the bats-support lib in your bats test:
 
             load '#{HOMEBREW_PREFIX}/lib/bats-support/load.bash'
       EOS
-    end
+  end
 
-    test do
+  test do
       ENV["TEST_DEPS_DIR"] = "#{HOMEBREW_PREFIX}/lib"
       system "bats", "#{lib}/bats-support/test"
-    end
+  end
 end
 
